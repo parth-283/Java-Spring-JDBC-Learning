a5dc9829b85341054345d019f1062614529a11f6
parth-283 <kp2832002@gmail.com>
2023-09-29 15:16:45 +0530

    Set AutoWiring.

--- src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -2,19 +2,24 @@ package com.spring.jdbc.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import com.spring.jdbc.entites.Student;
 
+@Component("studentDao")
 public class StudentDaoImpl implements StudentDao {
 
+//	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	public JdbcTemplate getJdbcTemplate() {
 		return jdbcTemplate;
 	}
 
+	@Autowired
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
